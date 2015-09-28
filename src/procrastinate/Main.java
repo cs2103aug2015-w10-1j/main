3e85705438d9b8625ed5adad61c73f3930f52be0
John Yong <whipermr5@gmail.com>
2015-09-28 15:47:48 +0800

    Removed unused imports and suppressed unused warning for overwriteDecorations

--- src/procrastinate/Main.java
+++ src/procrastinate/Main.java
@@ -5,10 +5,8 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -48,6 +46,7 @@ public class Main extends Application {
     }
 
     // Unused for now
+    @SuppressWarnings("unused")
     private void overwriteDecorations(Stage primaryStage, Parent root) {
         primaryStage.initStyle(StageStyle.UNDECORATED);
         root.setOnMousePressed(new EventHandler<MouseEvent>() {
