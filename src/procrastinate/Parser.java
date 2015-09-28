a06786601bb9b4f79b86e885c6b4f1a91006b4b1
John Yong <whipermr5@gmail.com>
2015-09-28 21:28:01 +0800

    Parser now uses switch statement

--- src/procrastinate/Parser.java
+++ src/procrastinate/Parser.java
@@ -4,15 +4,29 @@ public class Parser {
 
     private static final String DEBUG_PARSING_COMMAND = "Parsing command: ";
     private static final String COMMAND_EXIT = "exit";
+    private static final String COMMAND_PROCRASTINATE = "procrastinate";
 
     public static Command parse(String userCommand) {
         Utilities.printDebug(DEBUG_PARSING_COMMAND + userCommand);
 
-        if (userCommand.equalsIgnoreCase(COMMAND_EXIT)) {
-            return new Command(Command.Type.EXIT);
-        } else {
-            return new Command(Command.Type.ADD_DREAM).addDescription(userCommand);
+        userCommand = userCommand.trim(); // Trim leading and trailing whitespace
+        String firstWord = getFirstWord(userCommand).toLowerCase(); // Case insensitive
+
+        switch (firstWord) {
+
+            case COMMAND_EXIT:
+            case COMMAND_PROCRASTINATE:
+                return new Command(Command.Type.EXIT);
+
+            default:
+                return new Command(Command.Type.ADD_DREAM).addDescription(userCommand);
+
         }
+
+    }
+
+    private static String getFirstWord(String userCommand) {
+        return userCommand.split(" ")[0];
     }
 
 }
