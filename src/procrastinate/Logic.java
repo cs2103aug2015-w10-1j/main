c0738e3900e4dd1966ef1f82cd1ff743513f0cf8
John Yong <whipermr5@gmail.com>
2015-09-29 18:18:51 +0800

    Separated out Logic and UI components

--- src/procrastinate/Logic.java
+++ src/procrastinate/Logic.java
@@ -1,85 +1,59 @@
 package procrastinate;
 
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.util.converter.NumberStringConverter;
-
 public class Logic {
 
     // ================================================================================
-    // Message Strings
+    // Message strings
     // ================================================================================
-    private static final String MESSAGE_WELCOME = "What would you like to Procrastinate today?";
 
     private static final String FEEDBACK_ADD_DREAM = "Adding dream: ";
-
-    private static final String STATUS_READY = "Ready!";
-    private static final String STATUS_PREVIEW_COMMAND = "Preview command: ";
-
-    private static final String UI_NUMBER_SEPARATOR = ". ";
-
-    private static final String DEBUG_VIEW_LOADED = "View is now loaded!";
 
     private static final String ERROR_PARSER_UNKNOWN_COMMAND = "Error with parser: unknown command type returned";
 
     // ================================================================================
-    // Class Variables
+    // Class variables
     // ================================================================================
-    private IntegerProperty taskCount = new SimpleIntegerProperty(1);
-
-    private ObservableList<String> taskList = FXCollections.observableArrayList();
-
-    private StringProperty statusLabelText = new SimpleStringProperty();
-    private StringProperty taskCountFormatted = new SimpleStringProperty();
-    private StringProperty taskCountString = new SimpleStringProperty();
-    private StringProperty userInput = new SimpleStringProperty();
 
     private TaskEngine taskEngine;
+    private UI ui;
 
     // ================================================================================
-    // FXML Field Variables
+    // Singleton pattern
     // ================================================================================
-    @FXML private BorderPane mainBorderPane;
-    @FXML private Label statusLabel;
-    @FXML private ListView<String> taskListView;
-    @FXML private TextField userInputField;
 
-    public void initialize() {
-        Utilities.printDebug(DEBUG_VIEW_LOADED);
+    private static Logic logic;
 
-        initUI();
-        attachHandlersAndListeners();
-        initBinding();
-
+    private Logic() {
         initTaskEngine();
-
-        setStatus(STATUS_READY);
     }
 
-    private String executeCommand(String userCommand) {
+    public static Logic getInstance() {
+        if (logic == null) {
+            logic = new Logic();
+        }
+        return logic;
+    }
+
+    // UI handle
+    public void setUi(UI ui) {
+        this.ui = ui;
+    }
+
+    // ================================================================================
+    // Public logic methods
+    // ================================================================================
+
+    public String executeCommand(String userCommand) {
 
         Command command = Parser.parse(userCommand);
 
         switch (command.getType()) {
 
             case ADD_DREAM:
-            	String description = command.getDescription();
-            	Task newDream = new Task(description);
-            	taskEngine.add(newDream);
-                addDreamToTaskList(newDream);
+                String description = command.getDescription();
+                Task newDream = new Task(description);
+                taskEngine.add(newDream);
+                ui.addDreamToTaskList(newDream);
                 return FEEDBACK_ADD_DREAM + description;
 
             case EXIT:
@@ -92,76 +66,12 @@ public class Logic {
 
     }
 
-    private void addDreamToTaskList(Task task) {
-        taskList.add(taskCountFormatted.get() + task.getDescription());
-        taskCount.set(taskCount.get() + 1);
-        updateListView();
-    }
-
     // ================================================================================
     // Init methods
     // ================================================================================
 
     private void initTaskEngine() {
         taskEngine = new TaskEngine();
-    }
-
-    private void initUI() {
-        taskListView.setPlaceholder(new Label(MESSAGE_WELCOME));
-    }
-
-    private void attachHandlersAndListeners() {
-        userInputField.setOnKeyReleased(createKeyReleaseHandler());
-        userInputField.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue.trim().isEmpty()) {
-                setStatus(STATUS_READY);
-            } else {
-                setStatus(STATUS_PREVIEW_COMMAND + newValue);
-            }
-        });
-    }
-
-    private void initBinding() {
-        userInput.bindBidirectional(userInputField.textProperty());
-        statusLabelText.bindBidirectional(statusLabel.textProperty());
-        taskCountString.bindBidirectional(taskCount, new NumberStringConverter());
-        taskCountFormatted.bind(Bindings.concat(taskCountString).concat(UI_NUMBER_SEPARATOR));
-    }
-
-    private EventHandler<KeyEvent> createKeyReleaseHandler() {
-        return (KeyEvent keyEvent) -> {
-            if (keyEvent.getCode().equals(KeyCode.ENTER)) {
-                if (!getInput().trim().isEmpty()) {
-                    String userCommand = getInput();
-
-                    String feedback = executeCommand(userCommand);
-                    setStatus(feedback);
-                } else {
-                    setStatus(STATUS_READY);
-                }
-                clearInput();
-            }
-        };
-    }
-
-    // ================================================================================
-    // UI utility methods
-    // ================================================================================
-
-    private void updateListView() {
-        taskListView.setItems(taskList);
-    }
-
-    private void clearInput() {
-        userInputField.clear();
-    }
-
-    private String getInput() {
-        return userInput.get();
-    }
-
-    private void setStatus(String status) {
-        statusLabelText.set(status);
     }
 
 }
---
+++ src/procrastinate/UI.java
@@ -0,0 +1,144 @@
+package procrastinate;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.util.converter.NumberStringConverter;
+
+public class UI {
+
+    // ================================================================================
+    // Message strings
+    // ================================================================================
+
+    private static final String MESSAGE_WELCOME = "What would you like to Procrastinate today?";
+
+    private static final String STATUS_READY = "Ready!";
+    private static final String STATUS_PREVIEW_COMMAND = "Preview command: ";
+
+    private static final String UI_NUMBER_SEPARATOR = ". ";
+
+    private static final String DEBUG_VIEW_LOADED = "View is now loaded!";
+
+    // ================================================================================
+    // Class variables
+    // ================================================================================
+
+    private Logic logic;
+
+    private IntegerProperty taskCount = new SimpleIntegerProperty(1);
+
+    private ObservableList<String> taskList = FXCollections.observableArrayList();
+
+    private StringProperty statusLabelText = new SimpleStringProperty();
+    private StringProperty taskCountFormatted = new SimpleStringProperty();
+    private StringProperty taskCountString = new SimpleStringProperty();
+    private StringProperty userInput = new SimpleStringProperty();
+
+    // ================================================================================
+    // FXML field variables
+    // ================================================================================
+
+    @FXML private BorderPane mainBorderPane;
+    @FXML private Label statusLabel;
+    @FXML private ListView<String> taskListView;
+    @FXML private TextField userInputField;
+
+    public void initialize() {
+        Utilities.printDebug(DEBUG_VIEW_LOADED);
+
+        initLogic();
+
+        initUI();
+        attachHandlersAndListeners();
+        initBinding();
+
+        setStatus(STATUS_READY);
+    }
+
+    public void addDreamToTaskList(Task task) {
+        taskList.add(taskCountFormatted.get() + task.getDescription());
+        taskCount.set(taskCount.get() + 1);
+        updateListView();
+    }
+
+    // ================================================================================
+    // Init methods
+    // ================================================================================
+
+    private void initLogic() {
+        logic = Logic.getInstance();
+        logic.setUi(this);
+    }
+
+    private void initUI() {
+        taskListView.setPlaceholder(new Label(MESSAGE_WELCOME));
+    }
+
+    private void attachHandlersAndListeners() {
+        userInputField.setOnKeyReleased(createKeyReleaseHandler());
+        userInputField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.trim().isEmpty()) {
+                setStatus(STATUS_READY);
+            } else {
+                setStatus(STATUS_PREVIEW_COMMAND + newValue);
+            }
+        });
+    }
+
+    private void initBinding() {
+        userInput.bindBidirectional(userInputField.textProperty());
+        statusLabelText.bindBidirectional(statusLabel.textProperty());
+        taskCountString.bindBidirectional(taskCount, new NumberStringConverter());
+        taskCountFormatted.bind(Bindings.concat(taskCountString).concat(UI_NUMBER_SEPARATOR));
+    }
+
+    private EventHandler<KeyEvent> createKeyReleaseHandler() {
+        return (KeyEvent keyEvent) -> {
+            if (keyEvent.getCode().equals(KeyCode.ENTER)) {
+                if (!getInput().trim().isEmpty()) {
+                    String userCommand = getInput();
+
+                    String feedback = logic.executeCommand(userCommand);
+                    setStatus(feedback);
+                } else {
+                    setStatus(STATUS_READY);
+                }
+                clearInput();
+            }
+        };
+    }
+
+    // ================================================================================
+    // UI utility methods
+    // ================================================================================
+
+    private void updateListView() {
+        taskListView.setItems(taskList);
+    }
+
+    private void clearInput() {
+        userInputField.clear();
+    }
+
+    private String getInput() {
+        return userInput.get();
+    }
+
+    private void setStatus(String status) {
+        statusLabelText.set(status);
+    }
+
+}
