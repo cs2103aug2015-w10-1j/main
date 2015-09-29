aa57b2655c0dd41b3cdcb497205e94ed11587e4d
John Yong <whipermr5@gmail.com>
2015-09-30 04:39:19 +0800

    Dropped unnecessary "Task." prefix; added project-specific save actions (auto remove trailing whitespaces and unused imports)

--- src/procrastinate/Deadline.java
+++ src/procrastinate/Deadline.java
@@ -4,16 +4,16 @@ import java.util.Date;
 
 public class Deadline extends Task {
 	private Date date;
-	
+
 	public Deadline(String description, Date date) {
-		super(Task.TaskType.DEADLINE, description);
+		super(TaskType.DEADLINE, description);
 		this.date = date;
 	}
-	
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	
+
 	public Date getDate() {
 		return date;
 	}
--- src/procrastinate/Event.java
+++ src/procrastinate/Event.java
@@ -5,17 +5,17 @@ import java.util.Date;
 public class Event extends Task {
 	private Date startDate;
 	private Date endDate;
-	
+
 	public Event(String description, Date startDate, Date endDate) {
-		super(Task.TaskType.EVENT, description);
+		super(TaskType.EVENT, description);
 		this.startDate = startDate;
 		this.endDate = endDate;
 	}
-	
+
 	public Date getStartDate() {
 		return startDate;
 	}
-	
+
 	public Date getEndDate() {
 		return endDate;
 	}
