77803b666bb99964656ff4a053ef3747898c76f8
Tan Xue Si <tanxs93@gmail.com>
2015-10-01 10:19:52 +0800

    added systray implementation with test icon and exit function

--- src/procrastinate/Main.java
+++ src/procrastinate/Main.java
@@ -1,5 +1,6 @@
 package procrastinate;
 
+import java.awt.*;
 import java.io.IOException;
 
 import javafx.application.Application;
@@ -17,6 +18,8 @@ public class Main extends Application {
     private static final double WINDOW_HEIGHT = 600;
     private static final double WINDOW_MIN_HEIGHT = 600;
     private static double xOffset, yOffset;
+
+    private static final String ICON_IMAGE = "testicon.png";
 
     public static void main(String[] args) {
         launch(args);
@@ -36,6 +39,9 @@ public class Main extends Application {
 
     private void initPrimaryStage(Stage primaryStage, Parent root) {
         configurePrimaryStage(primaryStage, root);
+        if (checkSysTraySupport()) {
+            createSysTray(primaryStage);
+        }
         primaryStage.show();
     }
 
@@ -44,6 +50,39 @@ public class Main extends Application {
         primaryStage.setMinHeight(WINDOW_MIN_HEIGHT);
         primaryStage.setMinWidth(WINDOW_MIN_WIDTH);
         primaryStage.setScene(new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT));
+    }
+
+    private void createSysTray(Stage primaryStage) {
+        SystemTray sysTray = SystemTray.getSystemTray();
+        TrayIcon sysTrayIcon = createSysTrayIcon();
+        sysTrayIcon.setImageAutoSize(true);
+        PopupMenu sysTrayPopup = createSysTrayMenu(primaryStage);
+        sysTrayIcon.setPopupMenu(sysTrayPopup);
+        try {
+            sysTray.add(sysTrayIcon);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private TrayIcon createSysTrayIcon() {
+        // Load image as system tray icon image
+        Image iconImage = Toolkit.getDefaultToolkit().getImage(ICON_IMAGE);
+        return new TrayIcon(iconImage, WINDOW_TITLE);
+    }
+
+    private PopupMenu createSysTrayMenu(Stage primaryStage) {
+        PopupMenu menu = new PopupMenu();
+
+        MenuItem menuExit = new MenuItem("Exit");
+        menuExit.addActionListener(e -> System.exit(0));
+
+        menu.add(menuExit);
+        return menu;
+    }
+
+    private boolean checkSysTraySupport() {
+        return  SystemTray.isSupported();
     }
 
     // Unused for now
