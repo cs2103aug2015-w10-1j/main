4b750e8f1cff947e5f091a08eed8a2a781352654
HooDeLin <hoodelin0619@gmail.com>
2015-10-08 10:06:48 +0800

    Implement parser to handle adding deadlines

--- src/procrastinate/Parser.java
+++ src/procrastinate/Parser.java
@@ -1,13 +1,18 @@
 package procrastinate;
 
+import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import com.joestelmach.natty.DateGroup;
 
 import procrastinate.Command.CommandType;
 
 public class Parser {
 
     private static final Logger logger = Logger.getLogger(Parser.class.getName());
+    private static final com.joestelmach.natty.Parser dateParser = new com.joestelmach.natty.Parser();
 
     // ================================================================================
     // Message strings
@@ -38,7 +43,13 @@ public class Parser {
         logger.log(Level.FINE, DEBUG_PARSING_COMMAND + userCommand);
 
         userCommand = userCommand.trim(); // Trim leading and trailing whitespace
+        List<DateGroup> groups = dateParser.parse(userCommand);
         String firstWord = getFirstWord(userCommand).toLowerCase(); // Case insensitive
+        Date inputDate = null;
+        if(hasDates(groups)){
+        	inputDate = getDate(groups);
+        	userCommand = splitDatesFromUserCommand(groups, userCommand);
+        }
 
         switch (firstWord) {
 
@@ -52,7 +63,11 @@ public class Parser {
                         return new Command(CommandType.EDIT).addDescription(description).addLineNumber(lineNumber);
 
                     } catch (NumberFormatException e) { // So "edit something" is a dream
-                        return new Command(CommandType.ADD_DREAM).addDescription(userCommand);
+                    	if(hasDates(groups)){
+                    		return new Command(CommandType.ADD_DEADLINE).addDescription(userCommand).addDate(inputDate);
+                    	} else {
+                    		 return new Command(CommandType.ADD_DREAM).addDescription(userCommand);
+                    	}
                     } catch (Exception e) { // So "edit 1" is invalid (no description given)
                         return new Command(CommandType.INVALID).addDescription(MESSAGE_INVALID_EDIT_NO_DESCRIPTION);
                     }
@@ -69,7 +84,11 @@ public class Parser {
                     return new Command(CommandType.DELETE).addLineNumber(lineNumber);
 
                 } catch (NumberFormatException e) { // So "delete something" is a dream
-                    return new Command(CommandType.ADD_DREAM).addDescription(userCommand);
+                	if(hasDates(groups)){
+                		return new Command(CommandType.ADD_DEADLINE).addDescription(userCommand).addDate(inputDate);
+                	} else {
+                		 return new Command(CommandType.ADD_DREAM).addDescription(userCommand);
+                	}
                 } catch (Exception e) { // So "delete" is invalid (no line number given)
                     return new Command(CommandType.INVALID).addDescription(MESSAGE_INVALID_LINE_NUMBER);
                 }
@@ -79,7 +98,11 @@ public class Parser {
                 if (userCommand.equalsIgnoreCase(firstWord)) { // So "undo something" is a dream
                     return new Command(CommandType.UNDO);
                 } else {
-                    return new Command(CommandType.ADD_DREAM).addDescription(userCommand);
+                	if(hasDates(groups)){
+                		return new Command(CommandType.ADD_DEADLINE).addDescription(userCommand).addDate(inputDate);
+                	} else {
+                		 return new Command(CommandType.ADD_DREAM).addDescription(userCommand);
+                	}
                 }
 
             case COMMAND_DONE:
@@ -91,7 +114,11 @@ public class Parser {
                         return new Command(CommandType.DONE).addLineNumber(lineNumber);
 
                     } catch (NumberFormatException e) { // So "done something" is a dream
-                        return new Command(CommandType.ADD_DREAM).addDescription(userCommand);
+                    	if(hasDates(groups)){
+                    		return new Command(CommandType.ADD_DEADLINE).addDescription(userCommand).addDate(inputDate);
+                    	} else {
+                    		 return new Command(CommandType.ADD_DREAM).addDescription(userCommand);
+                    	}
                     }
                 } else { // So "done" is invalid (no line number given)
                     return new Command(CommandType.INVALID).addDescription(MESSAGE_INVALID_LINE_NUMBER);
@@ -102,12 +129,19 @@ public class Parser {
                 if (userCommand.equalsIgnoreCase(firstWord)) { // So "procrastinate something" is a dream
                     return new Command(CommandType.EXIT);
                 } else {
-                    return new Command(CommandType.ADD_DREAM).addDescription(userCommand);
+                	if(hasDates(groups)){
+                		return new Command(CommandType.ADD_DEADLINE).addDescription(userCommand).addDate(inputDate);
+                	} else {
+                		 return new Command(CommandType.ADD_DREAM).addDescription(userCommand);
+                	}
                 }
 
             default:
-                return new Command(CommandType.ADD_DREAM).addDescription(userCommand);
-
+            	if(hasDates(groups)){
+            		return new Command(CommandType.ADD_DEADLINE).addDescription(userCommand).addDate(inputDate);
+            	} else {
+            		 return new Command(CommandType.ADD_DREAM).addDescription(userCommand);
+            	}
         }
 
     }
@@ -118,6 +152,26 @@ public class Parser {
 
     private static String getFirstWord(String userCommand) {
         return userCommand.split(" ")[0];
+    }
+
+    private static boolean hasDates(List<DateGroup> groups){
+    	return !groups.isEmpty();
+    }
+
+    private static Date getDate(List<DateGroup> groups){
+    	Date date = groups.get(0).getDates().get(0);
+    	return date;
+    }
+
+    private static String splitDatesFromUserCommand(List<DateGroup> groups, String userCommand){
+    	int endIndex = groups.get(0).getPosition();
+    	String removedDateCommand = userCommand.substring(0, endIndex - 1);
+    	String[] temp = removedDateCommand.split("due");
+    	StringBuilder stringBuilder = new StringBuilder();
+    	for(int i = 0; i < temp.length; i ++){
+    		stringBuilder.append(temp[i]);
+    	}
+    	return stringBuilder.toString();
     }
 
 }
