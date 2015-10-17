dba055d4d386069f08fa1c443f57a6b017ea8f87
Tan Xue Si <tanxs93@gmail.com>
2015-10-18 00:54:20 +0800

    updated main and help screen views

--- src/procrastinate/ui/MainScreen.java
+++ src/procrastinate/ui/MainScreen.java
@@ -1,12 +1,44 @@
 package procrastinate.ui;
 
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+
 public class MainScreen extends CenterScreen {
+
+    protected Node overdue;
+    protected Node thisWeek;
+    protected Node future;
+    protected Node dreams;
+
+    protected CategoryBox overdueBox;
+    protected CategoryBox thisWeekBox;
+    protected CategoryBox futureBox;
+    protected CategoryBox dreamsBox;
+
+    @FXML VBox mainVBox;
 
     protected MainScreen(String filePath) {
         super(filePath);
+        createCategories();
     }
 
-    protected void say() {
-        System.out.println("Say something");
+    private void createCategories() {
+        // Create all the different categories(by time frame) for entries to go into
+        overdueBox = new CategoryBox("Overdue");
+        thisWeekBox = new CategoryBox("This Week");
+        futureBox = new CategoryBox("Future");
+        dreamsBox = new CategoryBox("Dreams");
+
+        this.overdue = overdueBox.getCategoryBox();
+        this.thisWeek = thisWeekBox.getCategoryBox();
+        this.future = futureBox.getCategoryBox();
+        this.dreams = dreamsBox.getCategoryBox();
+
+        // Set up for placement of boxes
+        mainVBox.setPadding(new Insets(10));    // Looks a bit weird if the boxes stick right next to the border
+        mainVBox.setSpacing(10);                // Spacing between boxes
+        mainVBox.getChildren().addAll(overdue,thisWeek,future,dreams);
     }
 }
