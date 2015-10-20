80dd8fa7b327e1c1e26e18325ee88e9ea259a726
John Yong <whipermr5@gmail.com>
2015-10-20 12:47:01 +0800

    Fixed splash image not loading when run from jar file

--- src/procrastinate/ui/HelpScreen.java
+++ src/procrastinate/ui/HelpScreen.java
@@ -2,12 +2,16 @@ package procrastinate.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class HelpScreen extends CenterScreen {
 
     // ================================================================================
     // Message strings
     // ================================================================================
+
+    private static final String LOCATION_SPLASH_IMAGE = "icon.png";
 
     private static final String MESSAGE_WELCOME = "What would you like to Procrastinate today?";
 
@@ -16,6 +20,7 @@ public class HelpScreen extends CenterScreen {
     // ================================================================================
 
     @FXML Label subtitleLabel;
+    @FXML ImageView splashImageView;
 
     // ================================================================================
     // HelpScreen methods
@@ -24,5 +29,6 @@ public class HelpScreen extends CenterScreen {
     protected HelpScreen(String filePath) {
         super(filePath);
         subtitleLabel.setText(MESSAGE_WELCOME);
+        splashImageView.setImage(new Image(getClass().getResourceAsStream(LOCATION_SPLASH_IMAGE)));
     }
 }
