9d79ab31c7b83bddc3c042352225fd577eb97711
John Yong <whipermr5@gmail.com>
2015-10-22 15:38:23 +0800

    Added JUnit suite to run all tests

---
+++ src/procrastinate/test/AllTests.java
@@ -0,0 +1,11 @@
+package procrastinate.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ FileHandlerTest.class, LogicTest.class, ParserTest.class, UITest.class })
+public class AllTests {
+
+}
