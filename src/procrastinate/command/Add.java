d8a5185ed711a90be61f67c36a443031a47350b9
Gerald <ngjingsiang@gmail.com>
2015-11-08 17:58:51 +0800

    New abstract superclass for Add and Edit

--- src/procrastinate/command/Add.java
+++ src/procrastinate/command/Add.java
@@ -1,11 +1,9 @@
 //@@author A0124321Y
 package procrastinate.command;
 
-import java.util.Date;
-
 import procrastinate.task.Task;
 
-public abstract class Add extends CleanCommand implements AlterTask {
+public abstract class Add extends Alter implements AlterTask {
     protected Task task;
     protected String description;
 
@@ -22,24 +20,5 @@ public abstract class Add extends CleanCommand implements AlterTask {
 
     public String getDescription() {
         return description;
-    }
-
-    @Override
-    public String formatDateTime(Date date) {
-        return dateTimeFormatter.format(date);
-    }
-
-    @Override
-    public String formatDate(Date date) {
-        return dateFormatter.format(date);
-    }
-
-    @Override
-    public String shorten(String description, int maxLength) {
-        if (description.length() <= maxLength) {
-            return description;
-        }
-
-        return description.substring(0, maxLength - 1) + FEEDBACK_ELLIPSIS;
     }
 }
---
+++ src/procrastinate/command/Alter.java
@@ -0,0 +1,29 @@
+package procrastinate.command;
+
+import java.util.Date;
+
+public abstract class Alter extends CleanCommand implements AlterTask {
+
+    public Alter(CommandType type) {
+        super(type);
+    }
+
+    @Override
+    public String formatDateTime(Date date) {
+        return dateTimeFormatter.format(date);
+    }
+
+    @Override
+    public String formatDate(Date date) {
+        return dateFormatter.format(date);
+    }
+
+    @Override
+    public String shorten(String description, int maxLength) {
+        if (description.length() <= maxLength) {
+            return description;
+        }
+
+        return description.substring(0, maxLength - 1) + FEEDBACK_ELLIPSIS;
+    }
+}
