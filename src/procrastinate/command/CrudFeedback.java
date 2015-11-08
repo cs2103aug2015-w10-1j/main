7922da79133ab4ccd0c0b384e66b98e8da76e6d1
Gerald <ngjingsiang@gmail.com>
2015-11-08 20:29:07 +0800

    Add feedback strings for delete and done

--- src/procrastinate/command/CrudFeedback.java
+++ src/procrastinate/command/CrudFeedback.java
@@ -9,13 +9,19 @@ public interface CrudFeedback extends Feedback {
     public static final String ADD_DREAM = "New dream: %1$s";
     public static final String ADD_DEADLINE = "New deadline: %1$s due %2$s";
     public static final String ADD_EVENT = "New event: %1$s %2$s to %3$s";
-    public static final String INVALID_RANGE = "Invalid dates: %2$s is before %1$s";
     public static final String FEEDBACK_ELLIPSIS = "...";
 
     public static final String EDIT_DREAM = "Edited #%1$s: %2$s";
     public static final String EDIT_DEADLINE = "Edited #%1$s: %2$s due %3$s";
     public static final String EDIT_EVENT = "Edited #%1$s: %2$s %3$s to %4$s";
     public static final String EDIT_PARTIAL = "Please specify the new description/date(s) or press tab";
+
+    public static final String DELETED = "Deleted %1$s: %2$s";
+
+    public static final String DONE = "Done %1$s: %2$s";
+    public static final String UNDONE = "Undone %1$s: %2$s";
+
+    public static final String INVALID_RANGE = "Invalid dates: %2$s is before %1$s";
     public static final String INVALID_LINE_NUMBER = "Invalid line number: %1$d";
 
     public static final String ERROR_SAVE_HEADER = "Could not save changes to file!";
