dbef2f94e7637334cdfe5c4a4fc5fc45d86e9f40
Gerald <ngjingsiang@gmail.com>
2015-11-08 20:15:03 +0800

    Moved getTask and isInvalid into superclass
    So that it is accessible by other classes

--- src/procrastinate/command/CrudCmd.java
+++ src/procrastinate/command/CrudCmd.java
@@ -1,6 +1,9 @@
 //@@author A0124321Y
 package procrastinate.command;
 
+import procrastinate.task.Task;
+import procrastinate.task.TaskEngine;
+
 public abstract class CrudCmd extends CleanCommand implements CrudFeedback {
     protected int lineNum;
 
@@ -10,5 +13,25 @@ public abstract class CrudCmd extends CleanCommand implements CrudFeedback {
 
     public int getLineNum() {
         return lineNum;
+    }
+
+    /**
+     * Retrieves a task from the list of task TaskEngine maintains
+     * List is 0-based index.
+     * @param lineNumber visible from UI i.e. 1-based index
+     * @return task
+     */
+    public Task getTask(int lineNumber, TaskEngine taskEngine) {
+        return taskEngine.getCurrentTaskList().get(lineNumber - 1);
+    }
+
+    /**
+     * Checks for line number validity
+     * @param lineNum
+     * @param taskEngine
+     * @return
+     */
+    public boolean isInvalid(int lineNum, TaskEngine taskEngine) {
+        return !(lineNum >= 1 && lineNum <= taskEngine.getCurrentTaskList().size());
     }
 }
--- src/procrastinate/command/Edit.java
+++ src/procrastinate/command/Edit.java
@@ -2,7 +2,6 @@
 package procrastinate.command;
 
 import procrastinate.task.Task;
-import procrastinate.task.TaskEngine;
 
 public abstract class Edit extends CrudCmd implements CrudFeedback {
     protected Task oldTask;
@@ -11,25 +10,5 @@ public abstract class Edit extends CrudCmd implements CrudFeedback {
     public Edit(CommandType type, int lineNum) {
         super(type);
         this.lineNum = lineNum;
-    }
-
-    /**
-     * Retrieves a task from the list of task TaskEngine maintains
-     * List is 0-based index.
-     * @param lineNumber visible from UI i.e. 1-based index
-     * @return task
-     */
-    public Task getTask(int lineNumber, TaskEngine taskEngine) {
-        return taskEngine.getCurrentTaskList().get(lineNumber - 1);
-    }
-
-    /**
-     * Checks for line number validity
-     * @param lineNum
-     * @param taskEngine
-     * @return
-     */
-    public boolean isInvalid(int lineNum, TaskEngine taskEngine) {
-        return !(lineNum >= 1 && lineNum <= taskEngine.getCurrentTaskList().size());
     }
 }
