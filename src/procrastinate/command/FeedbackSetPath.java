c2db3e0dd9bc02742d90c12372170b74365c676f
Gerald <ngjingsiang@gmail.com>
2015-11-09 18:56:22 +0800

    Removed redundant whitespace for SetPath command feedback

--- src/procrastinate/command/FeedbackSetPath.java
+++ src/procrastinate/command/FeedbackSetPath.java
@@ -1,7 +1,7 @@
 package procrastinate.command;
 
 public interface FeedbackSetPath extends Feedback {
-    public static final String SET_LOCATION = "Set save location to %1$s %2$s";
+    public static final String SET_LOCATION = "Set save location to %1$s%2$s";
 
     public static final String ERROR_SET_LOCATION_HEADER = "Could not set save location:";
     public static final String ERROR_SET_LOCATION_MESSAGE = "%1$s%2$s\n\n" + FEEDBACK_TRY_AGAIN;
